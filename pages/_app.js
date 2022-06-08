cf53d9027470636dfca1cd82632e59dbaf470f85
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-06-08 09:54:16 +0100

    1:01:11

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import Head from "next/head"
+import { MoralisProvider } from "react-moralis"
+import { Header } from "../components/Header"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="Buy and sell NFTs" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </>
+    )
 }
 
 export default MyApp
